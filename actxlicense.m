73cffb1ff11a7ba9b4d4ca4b06f06b5bdd094c89
Mirhossein-Mousavi <smmousavi135798642@gmail.com>
2024-12-27 12:50:22 +0330

    main page is done

---
+++ actxlicense.m
@@ -0,0 +1,16 @@
+function lic = actxlicense(progid)
+
+if strcmpi(progid, 'C1Chart2D8U.Control.1')
+lic = 'mshrD';
+return;
+end
+
+if strcmpi(progid, 'C1Chart3D8U.Control.1')
+lic = 'mshrD';
+return;
+end
+
+if strcmpi(progid, 'VSFlexGrid8.VSFlexGridN.1')
+lic = 'mshrD';
+return;
+end
